94df96739577a5ee553e2a287101e3ee71902efc
Samarth375-hub <kunala-mca23@cs.du.ac.in>
2024-07-20 17:05:57 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,7 @@
-mkdir -p ~//.streamlit/
+#!/bin/bash
 
-
-echo "\
-[server]\n\
-port = $PORT\n\
-enableCORS = false\n\
-headless = true\n\
-\n\
-" > ~/.streamlit/config.toml
-
+# Set environment variables for Streamlit
+export STREAMLIT_SERVER_PORT=$PORT
+export STREAMLIT_SERVER_HEADLESS=true
+export STREAMLIT_SERVER_ENABLECORS=false
+export STREAMLIT_SERVER_ENABLEWEBRTC=false
